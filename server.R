4e227aa1d63d468fb31ce6766b5bc6c7caade0c8
namrouche993 <42832704+namrouche993@users.noreply.github.com>
2020-08-11 10:15:34 +0200

    Update server.R

--- server.R
+++ server.R
@@ -1,9 +1,3 @@
-library(data.table)
-library(randomForest)
-
-# Read in the RF model
-model <- readRDS("model.rds")
-
 shinyServer(function(input, output, session) {
 
     output$distPlot <- renderPlot({
